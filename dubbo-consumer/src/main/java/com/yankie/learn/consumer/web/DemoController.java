a657d69d8106dbb3995876bcaea913f6e6103bee
zhuyanqing927 <zhu.yanqing927@126.com>
2019-10-09 19:50:40 +0800

    集成swagger

--- dubbo-consumer/src/main/java/com/yankie/learn/consumer/web/DemoController.java
+++ dubbo-consumer/src/main/java/com/yankie/learn/consumer/web/DemoController.java
@@ -1,9 +1,11 @@
 package com.yankie.learn.consumer.web;
 
 import com.yankie.learn.service.DemoService;
+import io.swagger.annotations.Api;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.*;
 
+@Api(description  = "测试通讯接口")
 @RestController("demoController")
 @RequestMapping(value = "/demo")
 public class DemoController {
--- dubbo-consumer/src/main/java/com/yankie/learn/consumer/web/ProductBrandController.java
+++ dubbo-consumer/src/main/java/com/yankie/learn/consumer/web/ProductBrandController.java
@@ -2,12 +2,14 @@ package com.yankie.learn.consumer.web;
 
 import com.yankie.learn.domain.ProductBrandDTO;
 import com.yankie.learn.service.ProductBrandService;
+import io.swagger.annotations.Api;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(description = "品牌接口")
 @RestController("productBrandController")
 @RequestMapping(value = "/productBrand")
 public class ProductBrandController {
